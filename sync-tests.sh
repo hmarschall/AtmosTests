520b8d50ae4267a1f7923d0f36b37a152c749d48
James Shaw <js102@zepler.net>
2015-06-12 14:59:40 +0100

    get inertialWaves test result matching melvin2010

--- sync-tests.sh
+++ sync-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-rm -r build/gravityWaves-*
+rm -r build/advection-*
 rm -r build/resting-*
+rm -r build/gravityWaves-*
+scp -prC jasmin3:~/data/AtmosTests/build/advection-\* build/
 scp -prC jasmin3:~/data/AtmosTests/build/resting-\* build/
 scp -prC jasmin3:~/data/AtmosTests/build/gravityWaves-\* build/
