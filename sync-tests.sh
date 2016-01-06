09f705e19d75ea0f72b712ba305d183b2a640880
James Shaw <js102@zepler.net>
2016-01-06 13:48:06 +0000

    get smarter about syncing, just sampleLine.dat at the moment

--- sync-tests.sh
+++ sync-tests.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-rm -r build/advection-*
-rm -r build/resting-*
-rm -r build/gravityWaves-*
-mkdir -p build
-scp -prC jasmin3:~/data/AtmosTests/build/advection-\* build/
-scp -prC jasmin3:~/data/AtmosTests/build/resting-\* build/
-scp -prC jasmin3:~/data/AtmosTests/build/gravityWaves-\* build/
+rm -r build/thermalAdvection-*
+
+function sampleLine {
+	mkdir -p build/$1/18000
+	scp -prC $2:~/data/AtmosTests/build/$1/18000/sampleLine.dat build/$1/18000/
+}
+
+sampleLine thermalAdvection-btf-250m-500dz jasmin3
+sampleLine thermalAdvection-btf-250m-300dz jasmin3
+sampleLine thermalAdvection-btf-250m-250dz jasmin3
+sampleLine thermalAdvection-btf-250m-200dz jasmin3
+sampleLine thermalAdvection-btf-250m-150dz jasmin3
+sampleLine thermalAdvection-btf-250m-125dz jasmin3
+sampleLine thermalAdvection-btf-250m-100dz jasmin3
+sampleLine thermalAdvection-btf-250m-75dz jasmin3
+sampleLine thermalAdvection-btf-250m-50dz jasmin3
+sampleLine thermalAdvection-cutCell-250m-500dz conifer
+sampleLine thermalAdvection-cutCell-250m-300dz conifer
+sampleLine thermalAdvection-cutCell-250m-250dz conifer
+sampleLine thermalAdvection-cutCell-250m-200dz conifer
+sampleLine thermalAdvection-cutCell-250m-150dz conifer
+sampleLine thermalAdvection-cutCell-250m-125dz conifer
+sampleLine thermalAdvection-cutCell-250m-100dz conifer
+sampleLine thermalAdvection-cutCell-250m-75dz conifer
