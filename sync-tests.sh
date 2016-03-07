191c1e8d4f0c8226e258203560a38ddf48a6b4ee
James Shaw <js102@zepler.net>
2016-03-07 09:31:54 +0000

    update sync-tests

--- sync-tests.sh
+++ sync-tests.sh
@@ -17,21 +17,43 @@ function resting {
 function gravityWaves-meshes {
 	rm -r build/gravityWaves-mesh-*
 	scp -prC ash:~/data/AtmosTests/build/gravityWaves-mesh-cutCell-250m-300dz build/
-#	scp -prC ash:~/data/AtmosTests/build/gravityWaves-mesh-cutCell-250m-250dz build/
-#	scp -prC ash:~/data/AtmosTests/build/gravityWaves-mesh-cutCell-250m-200dz build/
+	scp -prC ash:~/data/AtmosTests/build/gravityWaves-mesh-cutCell-250m-200dz build/
+	scp -prC ash:~/data/AtmosTests/build/gravityWaves-mesh-cutCell-250m-150dz build/
+}
+
+function gravityWaves {
+	rm -r build/gravityWaves-*
+	sampleLine gravityWaves-btf-250m-500dz ash
+	sampleLine gravityWaves-btf-250m-300dz ash
+	sampleLine gravityWaves-btf-250m-250dz ash
+	sampleLine gravityWaves-btf-250m-200dz ash
+	sampleLine gravityWaves-btf-250m-150dz ash
+	sampleLine gravityWaves-btf-250m-125dz ash
+	sampleLine gravityWaves-btf-250m-100dz conifer
+	sampleLine gravityWaves-btf-250m-75dz conifer
+#	sampleLine gravityWaves-btf-250m-50dz jasmin1
+	sampleLine gravityWaves-cutCell-250m-500dz ash
+	sampleLine gravityWaves-cutCell-250m-300dz ash
+	sampleLine gravityWaves-cutCell-250m-250dz ash
+	sampleLine gravityWaves-cutCell-250m-200dz ash
+	sampleLine gravityWaves-cutCell-250m-150dz ash
+	sampleLine gravityWaves-cutCell-250m-125dz ash
+	sampleLine gravityWaves-cutCell-250m-100dz ash
+#	sampleLine gravityWaves-cutCell-250m-75dz ash
+#	sampleLine gravityWaves-cutCell-250m-50dz jasmin1
 }
 
 function thermalAdvection {
 	rm -r build/thermalAdvection-*
-	sampleLine thermalAdvection-btf-250m-500dz ash
-	sampleLine thermalAdvection-btf-250m-300dz ash
-	sampleLine thermalAdvection-btf-250m-250dz ash
-	sampleLine thermalAdvection-btf-250m-200dz ash
-	sampleLine thermalAdvection-btf-250m-150dz ash
-	sampleLine thermalAdvection-btf-250m-125dz ash
-	sampleLine thermalAdvection-btf-250m-100dz ash
-	sampleLine thermalAdvection-btf-250m-75dz ash
-	sampleLine thermalAdvection-btf-250m-50dz ash
+	sampleLine thermalAdvection-btf-250m-500dz conifer
+	sampleLine thermalAdvection-btf-250m-300dz conifer
+	sampleLine thermalAdvection-btf-250m-250dz conifer
+	sampleLine thermalAdvection-btf-250m-200dz conifer
+	sampleLine thermalAdvection-btf-250m-150dz conifer
+	sampleLine thermalAdvection-btf-250m-125dz conifer
+	sampleLine thermalAdvection-btf-250m-100dz conifer
+	sampleLine thermalAdvection-btf-250m-75dz birch
+#	sampleLine thermalAdvection-btf-250m-50dz jasmin1
 	sampleLine thermalAdvection-cutCell-250m-500dz conifer
 	sampleLine thermalAdvection-cutCell-250m-300dz conifer
 	sampleLine thermalAdvection-cutCell-250m-250dz conifer
@@ -39,11 +61,14 @@ function thermalAdvection {
 	sampleLine thermalAdvection-cutCell-250m-150dz conifer
 	sampleLine thermalAdvection-cutCell-250m-125dz conifer
 	sampleLine thermalAdvection-cutCell-250m-100dz conifer
-	sampleLine thermalAdvection-cutCell-250m-75dz jasmin3
-	sampleLine thermalAdvection-cutCell-250m-50dz conifer
+	sampleLine thermalAdvection-cutCell-250m-75dz birch
+#	sampleLine thermalAdvection-cutCell-250m-50dz jasmin1
 }
 
 #advection
-resting
+#resting
+#gravityWaves
 #gravityWaves-meshes
 #thermalAdvection
+sampleLine gravityWaves-btf-250m-50dz birch
+sampleLine gravityWaves-cutCell-250m-50dz birch
