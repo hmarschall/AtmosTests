29b69387dcae14d65f126eec601f2e960757a6ea
James Shaw <js102@zepler.net>
2016-10-13 16:50:08 +0100

    don't sync tri meshes

--- sync-deformationSphere.sh
+++ sync-deformationSphere.sh
@@ -4,7 +4,5 @@ rm -rf build/deformationSphere*
 mkdir -p build
 scp -rCp $MACHINE:~/data/AtmosTests/build/deformationSphere-gaussians-nondiv-linearUpwind-hex-collated build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/deformationSphere-gaussians-nondiv-cubicUpwind-hex-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/deformationSphere-gaussians-nondiv-linearUpwind-tri-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/deformationSphere-gaussians-nondiv-cubicUpwind-tri-collated build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/deformationSphere-gaussians-nondiv-linearUpwind-quad-collated build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/deformationSphere-gaussians-nondiv-cubicUpwind-quad-collated build/
--- sync-solidBodySphere.sh
+++ sync-solidBodySphere.sh
@@ -4,7 +4,5 @@ rm -rf build/solidBodySphere*
 mkdir -p build
 scp -rCp $MACHINE:~/data/AtmosTests/build/solidBodySphere-noTilt-linearUpwind-hex-collated build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/solidBodySphere-noTilt-cubicUpwind-hex-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/solidBodySphere-noTilt-linearUpwind-tri-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/solidBodySphere-noTilt-cubicUpwind-tri-collated build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/solidBodySphere-noTilt-linearUpwind-quad-collated build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/solidBodySphere-noTilt-cubicUpwind-quad-collated build/
