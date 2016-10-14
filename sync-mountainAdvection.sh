a659734a6f6386ef3f6e628f067f54cf2724aee9
James Shaw <js102@zepler.net>
2016-10-14 17:10:39 +0100

    add mountainAdvection-1000, update samplePlane for OpenFOAM-4

--- sync-mountainAdvection.sh
+++ sync-mountainAdvection.sh
@@ -2,9 +2,4 @@
 MACHINE=ash
 rm -rf build/mountainAdvection*
 mkdir -p build
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-linearUpwind-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-cubicUpwind-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-linearUpwind-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-cubicUpwind-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-linearUpwind-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-cubicUpwind-collated build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-linearUpwind-1000 build/
