20a709e556c431d4e59d0b97ecef0e26eea14bf3
James Shaw <js102@zepler.net>
2016-10-17 15:50:59 +0100

    add more mountainAdvection meshes

--- sync-mountainAdvection.sh
+++ sync-mountainAdvection.sh
@@ -3,3 +3,11 @@ MACHINE=ash
 rm -rf build/mountainAdvection*
 mkdir -p build
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-linearUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-cubicUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-linearUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-cubicUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-linearUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-cubicUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-linearUpwind-collated build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-linearUpwind-collated build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-linearUpwind-collated build/
