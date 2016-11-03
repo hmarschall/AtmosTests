acaad9d6b6a399ed281f9e36d3b10abcab9a4ea7
James Shaw <js102@zepler.net>
2016-11-03 14:46:53 +0000

    collate errors by mountain height

--- bin/collate.py
+++ bin/collate.py
@@ -2,14 +2,15 @@
 import os
 import sys
 
-if len(sys.argv) < 4:
-    print("Usage: collate.py <prefix> <suffix> <dx's>", file=sys.stderr)
+if len(sys.argv) < 5:
+    print("Usage: collate.py <prefix> <suffix> <filename> <dx's>", file=sys.stderr)
     sys.exit(1)
 
 prefix = sys.argv[1]
 suffix = sys.argv[2]
-dxs = sys.argv[3:]
+filename = sys.argv[3]
+dxs = sys.argv[4:]
 
 for dx in dxs:
-    with open(os.path.join(prefix + dx, suffix), 'r') as fin:
+    with open(os.path.join(prefix + dx + suffix, filename), 'r') as fin:
             print(dx + " " + fin.read(), end="")
