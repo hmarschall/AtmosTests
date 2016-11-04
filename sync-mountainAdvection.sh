be7beeef260bd20dcb05bc02ea18a5a319fe9438
James Shaw <js102@zepler.net>
2016-11-04 14:32:40 +0000

    sync error-by-height collated data

--- sync-mountainAdvection.sh
+++ sync-mountainAdvection.sh
@@ -2,12 +2,18 @@
 MACHINE=ash
 rm -rf build/mountainAdvection*
 mkdir -p build
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-linearUpwind-1000 build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-cubicUpwind-1000 build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-linearUpwind-1000 build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-cubicUpwind-1000 build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-linearUpwind-1000 build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-cubicUpwind-1000 build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-linearUpwind-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-linearUpwind-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-linearUpwind-collated build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-5000-linearUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-5000-cubicUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-5000-linearUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-5000-cubicUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-5000-linearUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-5000-cubicUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-collated-linearUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-collated-cubicUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-collated-linearUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-collated-cubicUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-collated-linearUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-collated-cubicUpwind-1000 build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-6000-linearUpwind-collated build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-6000-linearUpwind-collated build/
+scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-6000-linearUpwind-collated build/
