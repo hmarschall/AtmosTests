f93db0fea452f4f03e0e57b94c8fbe09591f6db7
James Shaw <js102@zepler.net>
2016-11-18 09:07:43 +0000

    use explicitTimestepping option

---
+++ make-mountainAdvection.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+make build/mountainAdvection-btf-collated-{linear,cubic}Upwind-1000/10000/l{inf,2}errorT.txt
+make build/mountainAdvection-cutCell-collated-{linear,cubic}Upwind-1000/10000/l{inf,2}errorT.txt
+make build/mountainAdvection-slantedCell-collated-{linear,cubic}Upwind-1000/10000/l{inf,2}errorT.txt
+make mountainAdvection-btf-6000-linearUpwind-collated/0/maxdt.txt
+make mountainAdvection-cutCell-6000-linearUpwind-collated/0/maxdt.txt
+make mountainAdvection-slantedCell-6000-linearUpwind-collated/0/maxdt.txt
+make build/mountainAdvection-btf-6000-{linear,cubic}Upwind-collated/10000/l{2,inf}errorT.txt
+make build/mountainAdvection-cutCell-6000-{linear,cubic}Upwind-collated/10000/l{2,inf}errorT.txt
+make build/mountainAdvection-slantedCell-6000-{linear,cubic}Upwind-collated/10000/l{2,inf}errorT.txt
