2a15e0477979b5676d8743d82e876369aa47eb60
James Shaw <js102@zepler.net>
2016-11-21 14:30:55 +0000

    amend BTF mountainAdvection convergence timesteps

--- make-mountainAdvection.sh
+++ make-mountainAdvection.sh
@@ -5,6 +5,6 @@ make build/mountainAdvection-slantedCell-collated-{linear,cubic}Upwind-1000/1000
 make build/mountainAdvection-btf-6000-{linear,cubic}Upwind-collated/10000/l{2,inf}errorT.txt
 make build/mountainAdvection-cutCell-6000-{linear,cubic}Upwind-collated/10000/l{2,inf}errorT.txt
 make build/mountainAdvection-slantedCell-6000-{linear,cubic}Upwind-collated/10000/l{2,inf}errorT.txt
-make mountainAdvection-btf-6000-linearUpwind-collated/0/maxdt.txt
-make mountainAdvection-cutCell-6000-linearUpwind-collated/0/maxdt.txt
-make mountainAdvection-slantedCell-6000-linearUpwind-collated/0/maxdt.txt
+make build/mountainAdvection-btf-6000-linearUpwind-collated/0/maxdt.txt
+make build/mountainAdvection-cutCell-6000-linearUpwind-collated/0/maxdt.txt
+make build/mountainAdvection-slantedCell-6000-linearUpwind-collated/0/maxdt.txt
