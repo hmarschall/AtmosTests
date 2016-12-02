b5afa97ae43b8711a54ee61110762ce0f5f33bfd
James Shaw <js102@zepler.net>
2016-12-02 17:23:34 +0000

    add comments to sync-mountainAdvection

--- sync-mountainAdvection.sh
+++ sync-mountainAdvection.sh
@@ -2,6 +2,7 @@
 MACHINE=ash
 rm -rf build/mountainAdvection*
 mkdir -p build
+# error plots
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-5000-linearUpwind-1000 build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-5000-cubicUpwind-1000 build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-5000-linearUpwind-1000 build/
@@ -9,6 +10,7 @@ scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-5000-cubicUp
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-5000-linearUpwind-1000 build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-5000-cubicUpwind-1000 build/
 
+# analytic contours
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-5000-linearUpwind-1000-analyticTracer build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-5000-cubicUpwind-1000-analyticTracer build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-5000-linearUpwind-1000-analyticTracer build/
@@ -16,16 +18,20 @@ scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-5000-cubicUp
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-5000-linearUpwind-1000-analyticTracer build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-5000-cubicUpwind-1000-analyticTracer build/
 
+# error norms for various h0 
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-collated-linearUpwind-1000 build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-collated-cubicUpwind-1000 build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-collated-linearUpwind-1000 build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-collated-cubicUpwind-1000 build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-collated-linearUpwind-1000 build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-collated-cubicUpwind-1000 build/
+
+# maxdt for various mesh spacings
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-btf-6000-linearUpwind-collated build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-slantedCell-6000-linearUpwind-collated build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-cutCell-6000-linearUpwind-collated build/
 
+# mesh plots
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-mesh-btf-5000-1000 build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-mesh-cutCell-5000-1000 build/
 scp -rCp $MACHINE:~/data/AtmosTests/build/mountainAdvection-mesh-slantedCell-5000-1000 build/
