41f4039659c56bc28e0047a85108cd2aecf060bc
James Shaw <js102@zepler.net>
2016-12-05 14:29:43 +0000

    update h0=5000 timesteps

--- make-mountainAdvection.sh
+++ make-mountainAdvection.sh
@@ -2,9 +2,9 @@
 make build/mountainAdvection-btf-collated-{linear,cubic}Upwind-1000/10000/l{inf,2}errorT.txt
 make build/mountainAdvection-cutCell-collated-{linear,cubic}Upwind-1000/10000/l{inf,2}errorT.txt
 make build/mountainAdvection-slantedCell-collated-{linear,cubic}Upwind-1000/10000/l{inf,2}errorT.txt
-make build/mountainAdvection-btf-6000-{linear,cubic}Upwind-collated/10000/l{2,inf}errorT.txt
-make build/mountainAdvection-cutCell-6000-{linear,cubic}Upwind-collated/10000/l{2,inf}errorT.txt
-make build/mountainAdvection-slantedCell-6000-{linear,cubic}Upwind-collated/10000/l{2,inf}errorT.txt
+make build/mountainAdvection-btf-5000-{linear,cubic}Upwind-1000/10000/l{2,inf}errorT.txt
+make build/mountainAdvection-cutCell-5000-{linear,cubic}Upwind-1000/10000/l{2,inf}errorT.txt
+make build/mountainAdvection-slantedCell-5000-{linear,cubic}Upwind-1000/10000/l{2,inf}errorT.txt
 make build/mountainAdvection-btf-6000-linearUpwind-collated/0/maxdt.txt
 make build/mountainAdvection-cutCell-6000-linearUpwind-collated/0/maxdt.txt
 make build/mountainAdvection-slantedCell-6000-linearUpwind-collated/0/maxdt.txt
