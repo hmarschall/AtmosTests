c1e356982a1cc29d8abc3e08dc40aceee60d4deb
James Shaw <js102@zepler.net>
2016-12-30 07:40:58 +0000

    update sync scripts

--- sync-horizontalAdvection.sh
+++ sync-horizontalAdvection.sh
@@ -3,5 +3,5 @@ MACHINE=ash
 rm -rf build/horizontalAdvection*
 mkdir -p build
 
-scp -rCp $MACHINE:~/data/AtmosTests/build/horizontalAdvection-btf-linearUpwind-collated build/
-scp -rCp $MACHINE:~/data/AtmosTests/build/horizontalAdvection-btf-cubicUpwind-collated build/
+scp -rCp $MACHINE:~/data/AtmosTests.cubicFit-jcp-2017/build/horizontalAdvection-btf-linearUpwind-collated build/
+scp -rCp $MACHINE:~/data/AtmosTests.cubicFit-jcp-2017/build/horizontalAdvection-btf-cubicUpwind-collated build/
