81ee4d76c2d3d32bf0ebbd57fe756229246d0dc4
James Shaw <js102@zepler.net>
2017-07-26 11:55:08 +0100

    implement schaerAdvect error collation

--- generate.py
+++ generate.py
@@ -144,30 +144,30 @@ class AtmosTests:
                 'schaerAdvect-btf-linearUpwind-collated',
                 fvSchemes=os.path.join('src/schaerAdvect/linearUpwind'),
                 tests=[
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-5000-linearUpwind', meshBtf5000, timestep=40),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-2500-linearUpwind', meshBtf2500, timestep=20),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-2000-linearUpwind', meshBtf2000, timestep=16),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-1250-linearUpwind', meshBtf1250, timestep=10),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-1000-linearUpwind', meshBtf1000, timestep=8),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-667-linearUpwind', meshBtf667, timestep=5),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-500-linearUpwind', meshBtf500, timestep=4),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-333-linearUpwind', meshBtf333, timestep=2.5),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-250-linearUpwind', meshBtf250, timestep=2)
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-5000-linearUpwind', 5000, meshBtf5000, timestep=40),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-2500-linearUpwind', 2500, meshBtf2500, timestep=20),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-2000-linearUpwind', 2000, meshBtf2000, timestep=16),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-1250-linearUpwind', 1250, meshBtf1250, timestep=10),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-1000-linearUpwind', 1000, meshBtf1000, timestep=8),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-667-linearUpwind', 1000/3*2, meshBtf667, timestep=5),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-500-linearUpwind', 500, meshBtf500, timestep=4),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-333-linearUpwind', 1000/3, meshBtf333, timestep=2.5),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-250-linearUpwind', 250, meshBtf250, timestep=2)
         ])
 
         btfCubicFitCollated = schaerAdvect.collated(
                 'schaerAdvect-btf-cubicFit-collated',
                 fvSchemes=os.path.join('src/schaerAdvect/cubicFit'),
                 tests=[
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-5000-cubicFit', meshBtf5000, timestep=40),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-2500-cubicFit', meshBtf2500, timestep=20),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-2000-cubicFit', meshBtf2000, timestep=16),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-1250-cubicFit', meshBtf1250, timestep=10),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-1000-cubicFit', meshBtf1000, timestep=8),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-667-cubicFit', meshBtf667, timestep=5),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-500-cubicFit', meshBtf500, timestep=4),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-333-cubicFit', meshBtf333, timestep=2.5),
-                    SchaerAdvectCollated.Test('schaerAdvect-btf-250-cubicFit', meshBtf250, timestep=2)
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-5000-cubicFit', 5000, meshBtf5000, timestep=40),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-2500-cubicFit', 2500, meshBtf2500, timestep=20),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-2000-cubicFit', 2000, meshBtf2000, timestep=16),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-1250-cubicFit', 1250, meshBtf1250, timestep=10),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-1000-cubicFit', 1000, meshBtf1000, timestep=8),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-667-cubicFit', 1000/3*2, meshBtf667, timestep=5),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-500-cubicFit', 500, meshBtf500, timestep=4),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-333-cubicFit', 1000/3, meshBtf333, timestep=2.5),
+                    SchaerAdvectCollated.Test('schaerAdvect-btf-250-cubicFit', 250, meshBtf250, timestep=2)
         ])
 
         b.add(btfLinearUpwindCollated)
